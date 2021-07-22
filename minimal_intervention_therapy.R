6171e6c880b7aa1dcc4b0f7f433147fe12f2edad
chaimdavid <87780938+chaimdavid@users.noreply.github.com>
2021-07-22 21:22:00 +0300

    Update minimal_intervention_therapy.R

--- minimal_intervention_therapy.R
+++ minimal_intervention_therapy.R
@@ -1,4 +1,3 @@
-#3.4
 #Comparison of the four medicines' intervention to the genes' expression
 lessmat<-matrix( c(min(table(genemat[,2])),min(table(genemat[,3])),min(table(genemat[,4])),min(table(genemat[,5]))),1,4)
 colnames(lessmat)<-c("Therapy A","Therapy B","Therapy C","Therapy D")
